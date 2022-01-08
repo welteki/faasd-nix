68be504764ad0b8bf7d3bc806b91a9451b2545d5
Han Verstraete <welteki@pm.me>
2022-01-08 17:56:52 +0000

    Add queue configuration options

--- modules/core-services/queue-worker.nix
+++ modules/core-services/queue-worker.nix
@@ -1,16 +1,24 @@
-{ ... }:
-{
-  config.services.faasd.containers = {
-    queue-worker = {
+{ config, lib, ... }:
+let
+  inherit (lib) mkOption types;
+
+  cfg = config.services.faasd;
+
+  mkContainer =
+    { natsChannel ? "faas-request"
+    , maxInflight ? 1
+    , writeDebug ? false
+    }: {
       image = "ghcr.io/openfaas/queue-worker:0.12.2";
       environment = {
         faas_nats_address = "nats";
         faas_nats_port = 4222;
+        faas_nats_channel = natsChannel;
         gateway_invoke = "true";
         faas_gateway_address = "gateway";
         ack_wait = "5m5s";
-        max_inflight = 1;
-        write_debug = "false";
+        max_inflight = maxInflight;
+        write_debug = builtins.toString writeDebug;
         basic_auth = "true";
         secret_mount_path = "/run/secrets";
       };
@@ -31,5 +39,48 @@
         "nats"
       ];
     };
+
+  defaultQueueOpts = {
+    maxInflight = mkOption {
+      description = "Number of messages sent to queue worker and how many functions are invoked concurrently.";
+      type = types.int;
+      default = 1;
+    };
+
+    writeDebug = mkOption {
+      description = "Print verbose logs";
+      type = types.bool;
+      default = false;
+    };
+  };
+
+  queueOpts = { name, ... }: {
+    options = defaultQueueOpts // {
+      natsChannel = mkOption {
+        description = "Nats channel to use for the queue. Defaults to the queue name.";
+        type = types.str;
+        default = name;
+      };
+    };
+  };
+in
+{
+  options = {
+    services.faasd.defaultQueue = defaultQueueOpts;
+
+    services.faasd.queues = mkOption {
+      description = "";
+      type = types.attrsOf (types.submodule queueOpts);
+      default = { };
+    };
+  };
+
+  config = {
+    services.faasd.containers = (lib.mapAttrs'
+      (queueName: cfg: {
+        name = "${queueName}-worker";
+        value = mkContainer cfg;
+      })
+      cfg.queues) // { queue-worker = mkContainer cfg.defaultQueue; };
   };
 }
