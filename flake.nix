7a7f933246aeaeb548da1bd2b104c498591e67b4
Han Verstraete <welteki@pm.me>
2022-05-28 00:01:41 +0200

    Add OpenFaaS core images to packages

--- flake.nix
+++ flake.nix
@@ -43,7 +43,10 @@
             buildGoModule
             fetchFromGitHub
             makeWrapper
-            iptables;
+            iptables
+            dockerTools;
+
+          inherit (dockerTools) pullImage;
         in
         {
           faasd-containerd = prev.containerd.overrideAttrs (old: rec {
@@ -112,6 +115,43 @@
               cp ./resolv.conf $out/installation/resolv.conf
             '';
           };
+
+          openfaas-images = {
+            basic-auth = pullImage {
+              imageName = "ghcr.io/openfaas/basic-auth";
+              imageDigest = "sha256:e99c385f248ce9a6569bf262e3480ddbf317cd5982243fcc229ee1766d24eb5a";
+              finalImageTag = "0.21.4";
+              sha256 = "sha256-LyXZcK7bmc1B/QNdgvfNiL5t/5IpvnLKtFokBxRIMa4=";
+            };
+
+            gateway = pullImage {
+              imageName = "ghcr.io/openfaas/gateway";
+              imageDigest = "sha256:538cca5c2e569856dd21d348a9d265c5e55a8de12ff8d023f2940d8dab606be6";
+              finalImageTag = "0.21.4";
+              sha256 = "sha256-UqsI+uj2077n/XiE1BZpzccoKmMiu2g2F22E6G7ue2M=";
+            };
+
+            queue-worker = pullImage {
+              imageName = "ghcr.io/openfaas/queue-worker";
+              imageDigest = "sha256:dd69cc3d77c2e06df54ed2dddea384b6defc51ec35763a5ed377548fd30c6831";
+              finalImageTag = "0.12.2";
+              sha256 = "sha256-Rq5xPkEfkd0NrDcIb5YY4SfAsYlqvQxN7yXx0/01lJs=";
+            };
+
+            nats = pullImage {
+              imageName = "docker.io/library/nats-streaming";
+              imageDigest = "sha256:ba1be2cd913a1e9f1ffc9445e5c04c169db333819beb7204deb3bc7c29fde5a8";
+              finalImageTag = "0.22.0";
+              sha256 = "sha256-hUY3GAyAveHnpxHmlX9jkKQG8vzgJfuEqeNZB4bByqU=";
+            };
+
+            prometheus = pullImage {
+              imageName = "docker.io/prom/prometheus";
+              imageDigest = "sha256:907e20b3b0f8b0a76a33c088fe9827e8edc180e874bd2173c27089eade63d8b8";
+              finalImageTag = "v2.14.0";
+              sha256 = "sha256-OTGGUOPgx2k2QPG+r5kj4slOXzzs3JUwvGoKWFW4BDw=";
+            };
+          };
         };
 
       nixosModules.faasd = {
@@ -143,6 +183,12 @@
       {
         packages = {
           inherit (pkgs) faasd faasd-containerd faasd-cni-plugins;
+
+          basic-auth-image = pkgs.openfaas-images.basic-auth;
+          gateway-image = pkgs.openfaas-images.gateway;
+          queue-worker-image = pkgs.openfaas-images.queue-worker;
+          nats-image = pkgs.openfaas-images.nats;
+          prometheus-image = pkgs.openfaas-images.prometheus;
         };
 
         defaultPackage = self.packages.${system}.faasd;
--- modules/core-services/queue-worker.nix
+++ modules/core-services/queue-worker.nix
@@ -7,20 +7,13 @@ let
 
   boolToString = e: if e then "true" else "false";
 
-  queue-worker = pullImage {
-    imageName = "ghcr.io/openfaas/queue-worker";
-    imageDigest = "sha256:dd69cc3d77c2e06df54ed2dddea384b6defc51ec35763a5ed377548fd30c6831";
-    finalImageTag = "0.12.2";
-    sha256 = "sha256-Rq5xPkEfkd0NrDcIb5YY4SfAsYlqvQxN7yXx0/01lJs=";
-  };
-
   mkContainer =
     { natsChannel ? "faas-request"
     , maxInflight ? 1
     , writeDebug ? false
     }: {
       image = "ghcr.io/openfaas/queue-worker:0.12.2";
-      imageFile = mkIf cfg.seedCoreImages queue-worker;
+      imageFile = mkIf cfg.seedCoreImages pkgs.openfaas-images.queue-worker;
       environment = {
         faas_nats_address = "nats";
         faas_nats_port = 4222;
