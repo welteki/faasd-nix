12d90cf753b26465245ff7fc7b03b418090cfef6
Han Verstraete <welteki@pm.me>
2023-06-14 22:22:22 +0200

    Fix mismatch between gateway service image and seed image

--- modules/core-services/gateway.nix
+++ modules/core-services/gateway.nix
@@ -41,7 +41,7 @@ in
 
   config = {
     services.faasd.containers.gateway = {
-      image = "ghcr.io/openfaas/gateway:0.26.2";
+      image = "ghcr.io/openfaas/gateway:0.26.3";
       imageFile = mkIf cfg.seedCoreImages pkgs.openfaas-images.gateway;
       environment = {
         basic_auth = boolToString cfg.basicAuth.enable;
